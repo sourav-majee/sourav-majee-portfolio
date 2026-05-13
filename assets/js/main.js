a9fe41c6a71948f38dfd1c31fab5812c919dbd1b
Sourav-majee <majeesourav.mca@email.com>
2026-05-13 21:58:32 +0530

    Fixed favicon and contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,26 @@
 /**
-* Template Name: FolioOne
-* Template URL: https://bootstrapmade.com/folioone-bootstrap-portfolio-website-template/
-* Updated: Aug 23 2025 with Bootstrap v5.3.7
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+* Sourav Majee Portfolio Main JS
 */
 
-(function() {
+(function () {
   "use strict";
 
   /**
-   * Apply .scrolled class to the body as the page is scrolled down
+   * Apply .scrolled class on scroll
    */
   function toggleScrolled() {
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
-    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+
+    if (
+      !selectHeader.classList.contains('scroll-up-sticky') &&
+      !selectHeader.classList.contains('sticky-top') &&
+      !selectHeader.classList.contains('fixed-top')
+    ) return;
+
+    window.scrollY > 100
+      ? selectBody.classList.add('scrolled')
+      : selectBody.classList.remove('scrolled');
   }
 
   document.addEventListener('scroll', toggleScrolled);
@@ -27,36 +31,27 @@
    */
   const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
-  function mobileNavToogle() {
+  function mobileNavToggle() {
     document.querySelector('body').classList.toggle('mobile-nav-active');
+
     mobileNavToggleBtn.classList.toggle('bi-list');
     mobileNavToggleBtn.classList.toggle('bi-x');
   }
+
   if (mobileNavToggleBtn) {
-    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+    mobileNavToggleBtn.addEventListener('click', mobileNavToggle);
   }
 
   /**
-   * Hide mobile nav on same-page/hash links
+   * Hide mobile nav on nav link click
    */
   document.querySelectorAll('#navmenu a').forEach(navmenu => {
     navmenu.addEventListener('click', () => {
+
       if (document.querySelector('.mobile-nav-active')) {
-        mobileNavToogle();
+        mobileNavToggle();
       }
-    });
 
-  });
-
-  /**
-   * Toggle mobile nav dropdowns
-   */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
     });
   });
 
@@ -64,6 +59,7 @@
    * Preloader
    */
   const preloader = document.querySelector('#preloader');
+
   if (preloader) {
     window.addEventListener('load', () => {
       preloader.remove();
@@ -76,41 +72,62 @@
   let scrollTop = document.querySelector('.scroll-top');
 
   function toggleScrollTop() {
+
     if (scrollTop) {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+
+      window.scrollY > 100
+        ? scrollTop.classList.add('active')
+        : scrollTop.classList.remove('active');
+
     }
+
   }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
+
+  if (scrollTop) {
+
+    scrollTop.addEventListener('click', (e) => {
+
+      e.preventDefault();
+
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+
     });
-  });
+
+  }
 
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
 
   /**
-   * Animation on scroll function and init
+   * AOS Animation
    */
   function aosInit() {
+
     AOS.init({
       duration: 600,
       easing: 'ease-in-out',
       once: true,
       mirror: false
     });
+
   }
+
   window.addEventListener('load', aosInit);
 
   /**
-   * Init typed.js
+   * Typed.js
    */
   const selectTyped = document.querySelector('.typed');
+
   if (selectTyped) {
+
     let typed_strings = selectTyped.getAttribute('data-typed-items');
+
     typed_strings = typed_strings.split(',');
+
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
@@ -118,86 +135,128 @@
       backSpeed: 50,
       backDelay: 2000
     });
+
   }
 
   /**
-   * Animate the skills items on reveal
+   * Skills animation
    */
   let skillsAnimation = document.querySelectorAll('.skills-animation');
+
   skillsAnimation.forEach((item) => {
+
     new Waypoint({
+
       element: item,
+
       offset: '80%',
-      handler: function(direction) {
+
+      handler: function () {
+
         let progress = item.querySelectorAll('.progress .progress-bar');
+
         progress.forEach(el => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%';
+
+          el.style.width =
+            el.getAttribute('aria-valuenow') + '%';
+
         });
+
       }
+
     });
+
   });
 
   /**
-   * Initiate Pure Counter
-   */
-  new PureCounter();
-
-  /**
-   * Init swiper sliders
+   * Swiper sliders
    */
   function initSwiper() {
-    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
-      let config = JSON.parse(
-        swiperElement.querySelector(".swiper-config").innerHTML.trim()
-      );
 
-      if (swiperElement.classList.contains("swiper-tab")) {
-        initSwiperWithCustomPagination(swiperElement, config);
-      } else {
+    document.querySelectorAll(".init-swiper")
+      .forEach(function (swiperElement) {
+
+        let config = JSON.parse(
+          swiperElement
+            .querySelector(".swiper-config")
+            .innerHTML.trim()
+        );
+
         new Swiper(swiperElement, config);
-      }
-    });
+
+      });
+
   }
 
   window.addEventListener("load", initSwiper);
 
   /**
-   * Init isotope layout and filters
+   * Isotope portfolio filter
    */
-  document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
-    let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
-    let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
-    let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+  document.querySelectorAll('.isotope-layout')
+    .forEach(function (isotopeItem) {
 
-    let initIsotope;
-    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
-      initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
-        itemSelector: '.isotope-item',
-        layoutMode: layout,
-        filter: filter,
-        sortBy: sort
-      });
-    });
+      let layout =
+        isotopeItem.getAttribute('data-layout') ?? 'masonry';
 
-    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
-      filters.addEventListener('click', function() {
-        isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-        this.classList.add('filter-active');
-        initIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        if (typeof aosInit === 'function') {
-          aosInit();
+      let filter =
+        isotopeItem.getAttribute('data-default-filter') ?? '*';
+
+      let sort =
+        isotopeItem.getAttribute('data-sort') ?? 'original-order';
+
+      let initIsotope;
+
+      imagesLoaded(
+        isotopeItem.querySelector('.isotope-container'),
+
+        function () {
+
+          initIsotope = new Isotope(
+            isotopeItem.querySelector('.isotope-container'),
+
+            {
+              itemSelector: '.isotope-item',
+              layoutMode: layout,
+              filter: filter,
+              sortBy: sort
+            }
+
+          );
+
         }
-      }, false);
-    });
+      );
 
-  });
+      isotopeItem
+        .querySelectorAll('.isotope-filters li')
+        .forEach(function (filters) {
+
+          filters.addEventListener('click', function () {
+
+            isotopeItem
+              .querySelector('.isotope-filters .filter-active')
+              .classList.remove('filter-active');
+
+            this.classList.add('filter-active');
+
+            initIsotope.arrange({
+              filter: this.getAttribute('data-filter')
+            });
+
+            if (typeof aosInit === 'function') {
+              aosInit();
+            }
+
+          }, false);
+
+        });
+
+    });
 
   /**
-   * Initiate glightbox
+   * GLightbox
    */
-  const glightbox = GLightbox({
+  GLightbox({
     selector: '.glightbox'
   });
 
